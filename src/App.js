bc79fa14feea62447ca2b2bbf45b65dc08242433
marsis <logozynski@gmail.com>
2019-05-28 16:43:05 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,31 +2,36 @@ import React, {Component} from 'react';
 import ToDoItem from './ToDoItem'
 import todosData from './todosData'
 
-/*
-function App() {
-
-
-
-    return (
-        <div className="todo-list">
-            {todosArray}
-        </div>
-    );
-}
-*/
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            todosArray: todosData
+            character: {},
+            todosArray: todosData,
+            loading: false
         }
         this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            loading: true
+        })
+        fetch('https://swapi.co/api/people/1')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    character: data,
+                    loading: false
+                })
+            })
     }
 
     handleChange(id) {
         console.log('changed', id)
         this.setState(prevState => {
+
             const updatedTodos = prevState.todosArray.map(todo => {
                 if (todo.id === id ) {
                     todo.completed = !todo.completed
@@ -34,17 +39,22 @@ class App extends Component {
                 return todo
             });
             return {
-                todosArray: updatedTodos
+                todosArray: updatedTodos,
+
             }
         })
 
     }
 
     render() {
+        const text = this.state.loading ? 'Loading..' : this.state.character.name;
        const todosArray = this.state.todosArray.map(todo => <ToDoItem key={todo.id} props={todo} handleChange={this.handleChange} />)
         return (
+            <div>
             <div className="todo-list">
                 {todosArray}
+            </div>
+            <div>{text}</div>
             </div>
         );
     }
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -4,11 +4,12 @@ import React from 'react';
 
 function ToDoItem(props) {
 
+    let conditionClass = props.props.completed ? 'todo-completed' : 'todo-uncompleted'
     console.log(props)
     return (
         <div className="todo-item">
             <input type="checkbox" onChange={() => props.handleChange(props.props.id)} checked={props.props.completed}/>
-            <p>{props.props.text}</p>
+            <p className={conditionClass}>{props.props.text}</p>
         </div>
     )
 }
